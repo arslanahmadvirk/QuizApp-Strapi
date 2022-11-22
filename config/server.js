10135ba0e1a8fd6fc2fe2fe39b8fea9f2a9d3499
Arslan Ahmad Virk <arslanahmadv098@gmail.com>
2022-11-23 02:12:19 +0500

    update

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '046b775452ee107e35f2e6ff631b1983'),
+    },
   },
 });
--- src/api/quiz-question/services/quiz-question.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * quiz-question service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::quiz-question.quiz-question');
